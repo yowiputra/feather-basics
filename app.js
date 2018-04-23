df616cf3051139e86e6712b98035729b26467480
yowiputra <yowiputra@gmail.com>
2018-04-23 13:21:53 -0700

    turn app.js to API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
 
 class Messages {
   constructor() {
@@ -59,35 +60,28 @@ class Messages {
   }
 }
 
-const app = feathers();
+const app = express(feathers());
+
+// Turn on JSON body parsing for REST services
+app.use(express.json())
+// Turn on URL-encoded body parsing for REST services
+app.use(express.urlencoded({ extended: true }));
+// Set up REST transport using Express
+app.configure(express.rest());
 
 // Initialize the messages service by creating
 // a new instance of our class
 app.use('messages', new Messages());
 
-async function processMessages() {
-  app.service('messages').on('created', message => {
-    console.log('Created a new message', message);
-  });
+// Set up an error handler that gives us nicer errors
+app.use(express.errorHandler());
 
-  app.service('messages').on('removed', message => {
-    console.log('Deleted message', message);
-  });
+// Start the server on port 3030
+const server = app.listen(3030);
 
-  await app.service('messages').create({
-    text: 'First message'
-  });
+// Use the service to create a new message on the server
+app.service('messages').create({
+  text: 'Hello from the server'
+});
 
-  const lastMessage = await app.service('messages').create({
-    text: 'Second message'
-  });
-
-  // Remove the message we just created
-  await app.service('messages').remove(lastMessage.id);
-
-  const messageList = await app.service('messages').find();
-
-  console.log('Available messages', messageList);
-}
-
-processMessages();
+server.on('listening', () => console.log('Feathers REST API started at http://localhost:3030'));
